54c1427ab9fe914cc75cb579e3ea1509192e89d7
liyard-tls <mckinley55X@gmail.com>
2025-06-20 18:24:54 +0300

    updated dev config

--- update_deployments_testing.sh
+++ update_deployments_testing.sh
@@ -1,9 +1,9 @@
-slot deployments delete liyard-evolute-duel torii
-slot deployments delete liyard-evolute-duel katana
-slot deployments create liyard-evolute-duel katana --dev --dev.no-fee
+slot deployments delete dev-evolute-duel torii
+slot deployments delete dev-evolute-duel katana
+slot deployments create --team evolute dev-evolute-duel katana --dev --dev.no-fee
 sozo build --profile testing 
 sozo migrate --profile testing
-slot deployments create liyard-evolute-duel torii --config torii_config_testing.toml #--version 1.5.4-preview.0 
+slot deployments create --team evolute dev-evolute-duel torii --config torii_config_testing.toml #--version 1.5.4-preview.0 
 sozo inspect --profile testing
 #slot deployments logs liyard-dojo-starter torii -f
 
