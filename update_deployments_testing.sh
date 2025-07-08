307474a1bbf194c049ec2452241cd35b2afd5019
liyard-tls <mckinley55X@gmail.com>
2025-07-08 13:43:55 +0300

    fixed testing config

--- update_deployments_testing.sh
+++ update_deployments_testing.sh
@@ -1,9 +1,9 @@
 slot deployments delete dev-evolute-duel torii
 slot deployments delete dev-evolute-duel katana
-slot deployments create dev-evolute-duel evolute-duel --team evolute --tier pro katana --dev --dev.no-fee
+slot deployments create dev-evolute-duel --team evolute --tier pro katana --dev --dev.no-fee
 sozo build --profile testing 
 sozo migrate --profile testing
-slot deployments create dev-evolute-duel evolute-duel --team evolute --tier pro torii --config torii_config_testing.toml --version v1.6.0-alpha.1
+slot deployments create dev-evolute-duel --team evolute --tier pro torii --config torii_config_testing.toml --version v1.6.0-alpha.1
 sozo inspect --profile testing
 #slot deployments logs liyard-dojo-starter torii -f
 
