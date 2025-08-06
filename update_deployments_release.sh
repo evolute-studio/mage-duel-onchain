6cc39cf1619c9ab01a24324e55a9192319a9262b
liyard-tls <mckinley55X@gmail.com>
2025-08-06 13:09:39 +0300

    updated configs

--- update_deployments_release.sh
+++ update_deployments_release.sh
@@ -1,9 +1,9 @@
 slot deployments delete evolute-duel torii
 slot deployments delete evolute-duel katana
-slot deployments create evolute-duel --team evolute --tier pro katana --dev --dev.no-fee
+slot deployments create evolute-duel --team evolute --tier pro katana --config katana_config_release.toml
 sozo build --release --unity
 sozo migrate --release
-slot deployments create evolute-duel --team evolute --tier pro torii --config torii_config_release.toml --version v1.6.0-alpha.1
+slot deployments create evolute-duel --team evolute --tier pro torii --config torii_config_release.toml
 sozo inspect --release
 #slot deployments logs liyard-dojo-starter torii -f
 
